25548570f8a3ef0565f8a1e7df02bf6061eb0202
Abdul-Muizz1310 <abdulmuizz1310@outlook.com>
2023-10-19 19:07:51 +0500

    Chnages to home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Layout/header';
+import Home from './Pages/home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Home />
     </div>
   );
 }
