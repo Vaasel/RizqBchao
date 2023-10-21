2df0f6eb5587bff55ad6c807d2704b1222750e7f
haris-khan1596 <mhkhan.bese20seecs@seecs.edu.pk>
2023-10-21 14:09:31 +0500

    Setup Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Layout/header';
 import Home from './Pages/home';
+import Login from './Pages/login'; // Import your Login component
+import Signup from './Pages/signup'; // Import your Signup component
+
 function App() {
   return (
-    <div className="main">
+    <Router>
       <Header />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home page */}
+        <Route path="/login" element={<Login />} /> {/* Login page */}
+        <Route path="/signup" element={<Signup />} /> {/* Signup page */}
+      </Routes>
+    </Router>
   );
 }
 
