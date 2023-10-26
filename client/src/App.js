dda7b9fbed865138faffb8362e82fd25c4ddcab6
Abdul-Muizz1310 <abdulmuizz1310@outlook.com>
2023-10-26 19:45:00 +0500

    home-conflict_resolution

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './Layout/header';
-import Footer from "./Layout/footer";
+import Header from './components/header';
+import Footer from "./components/footer";
 import Home from './Pages/home';
 import Login from './Pages/login'; // Import your Login component
 import Signup from './Pages/signup'; // Import your Signup component
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Home page */}
-        <Route path="/login" element={<Login />} /> {/* Login page */}
-        <Route path="/signup" element={<Signup />} /> {/* Signup page */}
-      </Routes>
-      <Footer/>
-    </Router>
+    <div className='main'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home page */}
+          <Route path="/login" element={<Login />} /> {/* Login page */}
+          <Route path="/signup" element={<Signup />} /> {/* Signup page */}
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
