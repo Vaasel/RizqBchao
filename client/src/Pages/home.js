29f0222ac322377618e9212f93aeb219da71716e
Abdul-Muizz1310 <abdulmuizz1310@outlook.com>
2023-10-27 16:07:01 +0500

    re

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,15 +1,18 @@
+import './home.css';
 import Intro from '../layout/intro';
 import Volunteer from '../layout/volunteer';
 import Working from '../layout/working';
 import Doner from '../layout/doner';
+import Rewards from '../layout/rewards';
 
 const Home = () => {
   return (
     <>
-      <Intro />
-      <Doner />
+      <Intro     />
+      <Doner     />
       <Volunteer />
-      <Working />
+      <Working   />
+      <Rewards   />
     </>
   );
 };
---
+++ client/src/layout/rewards.jsx
@@ -0,0 +1,20 @@
+import './rewards.css';
+
+
+const Rewards = ()=>{
+    return(
+        <>  
+            <div className='reward-t'><h1><b>Share your food, earn rewards</b></h1></div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Rewards;
