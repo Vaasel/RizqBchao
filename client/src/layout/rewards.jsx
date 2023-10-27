5b0be5cfec829b6246fdaca07e55fb3251155dd8
Abdul-Muizz1310 <abdulmuizz1310@outlook.com>
2023-10-27 16:11:06 +0500

    home-r

--- client/src/layout/rewards.jsx
+++ client/src/layout/rewards.jsx
@@ -1,4 +1,5 @@
 import './rewards.css';
+import Button from '../components/button'
 
 
 const Rewards = ()=>{
@@ -7,7 +8,8 @@ const Rewards = ()=>{
             <div className='reward-t'><h1><b>Share your food, earn rewards</b></h1></div>
             <div>
                 <div>
-
+                    <p>Join our rewards program to earn points for every pound of food you share through our platform. Redeem your points for exclusive discounts and coupons at local businesses in your community. By sharing your surplus food, you're not only helping those in need, but you're also contributing to a more sustainable and equitable food system in your neighborhood. Sign up today to start earning rewards and making a positive impact in your local community.</p>
+                    <Button text={['Get Rewards']} />
                 </div>
                 <div>
 
