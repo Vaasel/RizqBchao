644529770c60f26eb3824269e36fe095457c05e9
Abdul-Muizz1310 <abdulmuizz1310@outlook.com>
2023-10-28 15:50:31 +0500

    home-t

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -4,15 +4,17 @@ import Volunteer from '../layout/volunteer';
 import Working from '../layout/working';
 import Doner from '../layout/doner';
 import Rewards from '../layout/rewards';
+import Testimonial from '../layout/testimonial';
 
 const Home = () => {
   return (
     <>
-      <Intro     />
-      <Doner     />
-      <Volunteer />
-      <Working   />
-      <Rewards   />
+      <Intro      />
+      <Doner      />
+      <Volunteer  />
+      <Working    />
+      <Rewards    />
+      <Testimonial/>
     </>
   );
 };
