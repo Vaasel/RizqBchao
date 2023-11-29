fe8111ddb074c162d3360a8c205fee5a6edf0a39
Abdul-Muizz1310 <abdulmuizz1310@outlook.com>
2023-11-29 16:26:16 +0500

    s

--- client/src/Pages/donationPage.js
+++ client/src/Pages/donationPage.js
@@ -1,19 +1,63 @@
 import "./donationPage.css";
 import donation from '../assets/donation.png';
+import React, { useState } from "react";
+import { Card, Form } from "react-bootstrap";
+import Button from "../components/button";
 
 const Donation =()=>{
-    return(
-        <>
-        <div>
-            <div>
-                <img src={donation} alt="DonationImage" ></img>
-            </div>
-            <div>
 
-            </div>
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    return (
+      <>
+        <div className="Donate-1">
+          <div className="Donate-1-1">
+            <img src={donation} alt="DonationImage"></img>
+          </div>
+          <div>
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  <h1>Sign Up</h1>
+                </Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      placeholder="Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      type="email"
+                      value={email}
+                      placeholder="Email"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Form.Group>
+                  <div>
+                    <Button text={["Sign Up"]} />
+                  </div>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
-        </>
-    )
+      </>
+    );
 }
 
 export default Donation;
